0ecafb1d1313b1fe03b954b7949de2b775c72778
mouldimus <thomas@sportstore.co.uk>
2023-07-26 15:29:13 +0100

    Two Part Assignment - Part 2

--- Basic_HTML_and_JavaScript_1/Basic_HTML_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_HTML_1.js
@@ -1,1 +1,7 @@
 alert("Hello, World!");
+
+function My_First_Function() {        // A function that applies a green font color to the text "Kiss me, I'm Irish!"
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
